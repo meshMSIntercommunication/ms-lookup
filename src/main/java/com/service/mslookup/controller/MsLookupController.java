59dc22fd58bf07f9650fefe764a3867f32945fa2
charley43613 <41710701+charley43613@users.noreply.github.com>
2022-11-03 15:19:22 -0400

    Update MsLookupController.java

--- src/main/java/com/service/mslookup/controller/MsLookupController.java
+++ src/main/java/com/service/mslookup/controller/MsLookupController.java
@@ -20,82 +20,19 @@ import com.service.mslookup.models.*;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+
 
 
 @CrossOrigin
 @RestController
 public class MsLookupController {
   
-     @Autowired
-    private JdbcTemplate jdbcTemplate;
+ 
 
-    private final Logger log = LoggerFactory.getLogger(SampleController.class);
-    MathContext m = new MathContext(5);
-    
-    private BigDecimal initValue(Double val) {
-        return new BigDecimal(val).round(m);
-    }
+
     @Operation(summary = "Put your endpoint description for swagger here. Do this for each endpoint.")
-     @GetMapping("/")
-     public String listSample() {
-		String sql = "SELECT * FROM REV_LINE_ITEM";
-		List<Map<String, Object>> sqlResults = jdbcTemplate.queryForList(sql);
-		Map<Integer, ArrayList<BigDecimal>> results = new HashMap<Integer, ArrayList<BigDecimal>>();
-		
-		Integer i = 1;
-		for(Map<String, Object> o : sqlResults) {
-			results.put(i++,  new ArrayList<BigDecimal>(Arrays.asList(
-				initValue((Double)o.get("P1")),
-				initValue((Double)o.get("P2")),
-				initValue((Double)o.get("P3")),
-				initValue((Double)o.get("P4")),
-				initValue((Double)o.get("P5")),
-				initValue((Double)o.get("P6")),
-				initValue((Double)o.get("P7")),
-				initValue((Double)o.get("P8")),
-				initValue((Double)o.get("P9")),
-				initValue((Double)o.get("P10"))
-        	)));
-  		}
-
-		// map each period year to the sum of its list of amounts
-		Map<Integer, BigDecimal> totals = new HashMap<>();
-
-		// sum the values
-		for(Integer k : results.keySet()) {
-			BigDecimal sum = BigDecimal.ZERO;
-			for(BigDecimal d : results.get(k)) {
-				sum = sum.add(d);
-			}
-			totals.put(k, sum.round(m));
-		}
-		String output = "";
-		String lineBreak = "<br>";
-		output += "Sample Recalc 7.3" + lineBreak + lineBreak;
-		output += "Input" + lineBreak;
-
-		for(Integer k : totals.keySet()) {
-			output += "Period Id = " + k.toString() + lineBreak;
-			for(BigDecimal d : results.get(k)) {
-				output += "Esn Revenue = " + d.toString() + lineBreak;
-			}
-		}
-
-		output += lineBreak + "Output" + lineBreak;
-
-		for(Integer k : totals.keySet()) {
-			output += "Period Id = " + k.toString();
-			output += ", Esn Summed Revenue = " + totals.get(k).toString() + lineBreak;
-		}
-
-		return output;
-	}
-	
-	  		//check for VALUE equal to requesting user generating new ms
 	    @PostMapping("/msNameCheck")
 	    public List<LookupResponse> checkMsName(@RequestParam("msNameRequested") String msNameRequested) {
 	        Map<String, String> env = System.getenv();
